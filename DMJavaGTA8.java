8241539486488a68973e6068e9744636508c0c73
Dean27817 <79397523+Dean27817@users.noreply.github.com>
2023-04-10 17:21:17 -0600

    Add files via upload

--- DMJavaGTA8.java
+++ DMJavaGTA8.java
@@ -1,7 +1,21 @@
-///////////////////////////////////////////////////////////////////////////////need to add comments still
+/*Author: dean morgan
+ * date: 4/9/2023
+ * class: csc 160
+ * assignment 
+ * this will be a program to show compotince and understanding with 2D arrays and sorting algotithems
+ *  
+ */
 
 public class DMJavaGTA8 {
 
+
+    /*  Author:	Dean Morgan
+**  Date:	4/9/2023
+**  Description:	sorts an array
+**  Parameters:	array1 (int array)
+**  Calling method:	main
+**  Return values:   none
+*/
     public static void arraySorter(int[] array1) {
         int holder;
         for (int i = 0; i < array1.length; i++)
@@ -22,7 +36,6 @@ public class DMJavaGTA8 {
         }
     }
 
-
     public static void main(String[] args) {
         int[] array1 = {22, 54, 2, 10, 19, 104, 16, 73, 88, 5};
         for (int i = 0; i < array1.length; i++)
@@ -31,6 +44,7 @@ public class DMJavaGTA8 {
         }
         System.out.println("now sorted");
         arraySorter(array1);
+        System.out.println("it is called a bianry search because the it only looks at two items at a time");
         //////////////////////////////////////////////////////////////////////////Part One
 
         int[][] num = {{1 , 2, 3, 4, 5, 6, 7, 8, 9, 10}, {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}, {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000}, {1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000}, {10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000}};
@@ -49,7 +63,7 @@ public class DMJavaGTA8 {
         System.out.println(planetArray[0][1]);
         /////////////////////////////////////////////////////////////////////////////////////Part Two
 
-        
+
     }
 }
 
